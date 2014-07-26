d07f6147413f3078903d42234217b721f0a58ec0
RwllCnlpn <rowell.dcanlapan@gmail.com>
2014-07-26 20:59:17 +0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	function scrollToAnchor(aid){
+	    var aTag = $("a[name='"+ aid +"']");
+	    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+	}
+
+	$('#button1').click(function(){
+		scrollToAnchor("first");
+	});
+
+	$('#button2').click(function(){
+		scrollToAnchor("second");
+	});
+
+	$('#button3').click(function(){
+		scrollToAnchor("third");
+	});
+
+	$('#button4').click(function(){
+		scrollToAnchor("fourth");
+	});
+
+	$('#button5').click(function(){
+		scrollToAnchor("fifth");
+	});
+
+	$('#button6').click(function(){
+		scrollToAnchor("sixth");
+	});
+
+});
